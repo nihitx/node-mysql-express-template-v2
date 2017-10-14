4c8f69cb2b30f18467387513e19c8715c2225ac5
nihitx <masnadhossain@live.com>
2017-10-14 23:17:36 +0300

    circle ci push

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 var mysql = require('mysql');
+var env = process.env.NODE_ENV || 'development';
+if(env=="development"){
+  var connection = mysql.createConnection({
+      host     : '127.0.0.1', // Your host - either local or cloud
+      user     : 'root', // your username
+      password : 'root', // your password
+      database : 'your-db-name' // database name
+  });
+}else{
+  var connection = mysql.createConnection({
+        host     : 'us-cdbr-iron-east-05.cleardb.net',
+        user     : 'b0350d64c4deb3',
+        password : 'ae902617',
+        database : 'heroku_98b4d00635d887c'
+    });
+}
 
-var connection = mysql.createConnection({
-    host     : '127.0.0.1', // Your host - either local or cloud
-    user     : 'root', // your username
-    password : 'root', // your password
-    database : 'your-db-name' // database name
-});
 connection.connect(function(err) {
     if (err) throw err;
 });
